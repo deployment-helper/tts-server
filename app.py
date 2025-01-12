853ba2743dcb37426a9ab2aa8e2786d0044f0968
vinaymavi <vinaymavi@gmail.com>
2025-01-12 12:10:31 +0530

    Semaphore setting added to limit concurrent requests count.

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import base64
 import logging
 import io
+from threading import Semaphore
 from TTS.utils.synthesizer import Synthesizer
 from TTS.utils.manage import ModelManager
 from flask import Flask, render_template, request, send_file, jsonify
 
 XTTS_V2 = "tts_models/multilingual/multi-dataset/xtts_v2"
+MAX_REQUEST_SIZE = 1
 
+sem = Semaphore(MAX_REQUEST_SIZE)
 manager = ModelManager('./.models.json')
 model_path, config_path, model_item = manager.download_model(XTTS_V2)
 
@@ -42,27 +45,31 @@ def api_models():
 
     return jsonify(manager.list_tts_models())
 
-@app.route('/api/synthesize', methods=['POST'])
+@app.route('/api/synthesize', methods=['POST','OPTIONS'])
 def api_synthesize():
     data = request.get_json()
     text = data['text']
     send_as_file = data.get('send_as_file', False)
-    speaker = data.get('speaker', None)
-    language = data.get('language', None)
+    speaker = data.get('speaker', 'Tanja Adelina')
+    language = data.get('language', 'en')
     logging.log(logging.INFO, f"Synthesizing: {text}")
-    wavs = synthesizer.tts(text, language_name=language, speaker_name=speaker)
-    out = io.BytesIO()
-    synthesizer.save_wav(wavs, out)
-    if send_as_file:
-        out.seek(0)
-        return send_file(out, mimetype="audio/wav")
-    else:
-        # return as base64
-        out.seek(0)
-        audio_content = base64.b64encode(out.getvalue()).decode('utf-8')
-        logging.log(logging.INFO, f"Synthesized: {audio_content}")
-        return jsonify({"audioContent": audio_content})
+
+    sem.acquire()
+    try:
+        wavs = synthesizer.tts(text, language_name=language, speaker_name=speaker)
+        out = io.BytesIO()
+        synthesizer.save_wav(wavs, out)
+        if send_as_file:
+            out.seek(0)
+            return send_file(out, mimetype="audio/wav")
+        else:
+            out.seek(0)
+            audio_content = base64.b64encode(out.getvalue()).decode('utf-8')
+            logging.log(logging.INFO, f"Synthesized: {audio_content}")
+            return jsonify({"audioContent": audio_content})
+    finally:
+        sem.release()
 # Main entry point
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    app.run(debug=True)
+    app.run(debug=True,threaded=True)
